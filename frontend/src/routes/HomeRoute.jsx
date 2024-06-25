16aa3c63d78c9920b2eaa9506e3097b39f6c539a
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-06-25 08:17:26 -0400

    create  the HomeRoute component of the PhotoLabs app

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import TopNavigationBar from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = ({ getAllPhotos, isLiked, toggleLike, photos, topics, getPhotosByTopic }) => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigationBar getAllPhotos={getAllPhotos} topics={topics} getPhotosByTopic={getPhotosByTopic}/>
+      <PhotoList isLiked={isLiked} toggleLike={toggleLike} photos={photos}/>
     </div>
   );
 };
