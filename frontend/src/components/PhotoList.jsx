3e665c3e0dbb245d89f3bbbae2db59f71921772f
Ahmad <36176055+ItsAhmad@users.noreply.github.com>
2024-07-30 17:13:36 -0400

    fix typo from photosArray to photoArray

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -7,7 +7,7 @@ Component to retrieve details about multiple photo stored in our database
 */ 
 
 const PhotoList = ({ isLiked, toggleLike, photos, showModal }) => {
-  const photosArray = object.values(similar_photos); 
+  const photoArray = object.values(similar_photos); 
 
   /* if (Array.isArray(photos)) {
     photoArray = photos;
