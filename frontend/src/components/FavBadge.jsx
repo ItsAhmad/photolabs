f588af01cd619cc6fbc5382eeb7fab0a90d3dac4
Ahmad <36176055+ItsAhmad@users.noreply.github.com>
2024-07-30 10:28:40 -0400

    Add comments to FavBadge component

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/FavBadge.scss';
 
+/* 
+Displays a badge on the favourites button, only if photos are liked by user
+*/
+
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
     <div className='fav-badge'>
