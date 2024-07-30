6456d0554855fe4f81c888cd8a3c8630cfc382a6
Ahmad <36176055+ItsAhmad@users.noreply.github.com>
2024-07-30 11:24:19 -0400

    add comments

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "../styles/TopicListItem.scss";
 
+/*
+Component to pull relevant photos by topic
+*/
+
 const TopicListItem = ({ topic, getPhotosByTopic }) => {
   return (
     <div className="topic-list__item" onClick={() => getPhotosByTopic(topic.id)}>
