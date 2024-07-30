30d77dd1bfea1a909da465d9a2435b5162dadb9d
Ahmad <36176055+ItsAhmad@users.noreply.github.com>
2024-07-30 11:18:03 -0400

    clean up for code readability, added comments

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,17 +2,32 @@ import React from "react";
 import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
 
+
+/* 
+Component to display information with each picture
+*/
+
+
 const PhotoListItem = ({ isLiked, toggleLike, photoId, photo, showModal }) => {
   const { urls, user, location } = photo;
 
   return (
     <div className={`photo-list__item`}>
       <PhotoFavButton isLiked={isLiked} toggleLike={toggleLike} photoId={photoId}></PhotoFavButton>
-      <img className="photo-list__image" src={urls.regular} alt={`Image taken in ${location.city}, ${location.country}`} onClick={showModal}></img>
+      <img className="photo-list__image" 
+           src={urls.regular} 
+           alt={`Image taken in ${location.city}, ${location.country}`} 
+           onClick={showModal}></img>
+      
       <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={user.profile} alt={`Profile for ${user.username}`}></img>
+        <img className="photo-list__user-profile" 
+             src={user.profile} 
+             alt={`Profile for ${user.username}`}>
+       </img>
+
         <div className="photo-list__user-info">
           <span>{user.name}</span>
+
           <div className="photo-list__user-location">
             {location.city}, {location.country}
           </div>
